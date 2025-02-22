5c75060f77688228a86e98b87083ba9c6a1cad29
Jayashanker Padishala <jayashankar.pjs@gmail.com>
2025-02-22 10:57:53 -0800

    Initial commit

---
+++ test/family-tree-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FamilyTreeInfra from '../lib/family-tree-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/family-tree-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FamilyTreeInfra.FamilyTreeInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
